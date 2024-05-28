307890e81bc1edfc7880a3c4dbc8e660d7d872c5
timcsy <timocsy@yahoo.com.tw>
2024-05-28 12:46:24 +0800

    add record

--- src/eventstreaming/websocket/stream.py
+++ src/eventstreaming/websocket/stream.py
@@ -56,12 +56,14 @@ def get_io_event():
         return None
     return input_queue.get_nowait()
 
-def get_io_events():
+def get_io_events(timestamp=True):
     input_events = []
     event = get_io_event()
     while event is not None:
         input_events.append(event)
         event = get_io_event()
+    if not timestamp:
+        return list(map(lambda x: x['event'], input_events))
     return input_events
 
 def send_io_event(event):
@@ -69,6 +71,56 @@ def send_io_event(event):
         'event': event,
         'timestamp': int((time.time() - start_time) * 1_000_000) 
     }))
+
+def record_io_event(path=None):
+    inputs = []
+    tmp_inputs = []
+    state = 'start'
+    while True:
+        input = get_io_event()
+        while input is None:
+            time.sleep(0.001)
+            input = get_io_event()
+        if state =='start' and input['event'] == 'p ctrl':
+            state = 'p ctrl'
+            tmp_inputs.append(input)
+        elif state =='p ctrl' and input['event'] == 'pr':
+            state = 'end'
+            break
+        else:
+            if state != 'end' and len(tmp_inputs) > 0:
+                inputs.extend(tmp_inputs)
+                tmp_inputs = []
+            state = 'start'
+            inputs.append(input)
+    if path is None:
+        print(inputs)
+    else:
+        with open(path, 'w') as fout:
+            json.dump(inputs, fout, indent=4)
+
+def play_io_event(records=None):
+    record_queue = queue.Queue()
+
+    record_list = []
+    if type(records) == str:
+        with open(records, 'r') as fin:
+            record_list = json.load(fin)
+    elif type(records) == list:
+        record_list = records
+    for record in record_list:
+        record_queue.put(record)
+    
+    start_time = time.time()
+    record = record_queue.get_nowait()
+    while record is not None:
+        event_time = record['timestamp'] / 1_000_000.0
+        if time.time() >= start_time + event_time:
+            send_io_event(record['event'])
+            if record_queue.empty():
+                record = None
+            else:
+                record = record_queue.get_nowait()
 
 def start(waiting_secs=0.5, screen=True, left=None, top=None, width=None, height=None, new_width=None, new_height=None, fullscreen=False):
     global start_time, ws, ws_thread, has_screen, terminate
---
+++ tests/record.py
@@ -0,0 +1,8 @@
+from eventstreaming import stream
+
+stream.start()
+
+stream.record_io_event('record.json')
+stream.play_io_event('record.json')
+
+stream.close()
